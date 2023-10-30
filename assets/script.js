885009884f0a21e41370818b079289b712e7899b
Bruno L <143929120+Darkitus@users.noreply.github.com>
2023-10-30 01:41:10 +0100

    modif boucles, ajout bullets points

--- assets/script.js
+++ assets/script.js
@@ -1,118 +1,73 @@
 const slides = [
-	{
-		"image":"./assets/images/slideshow/slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"./assets/images/slideshow/slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"./assets/images/slideshow/slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"./assets/images/slideshow/slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
+  {
+    image: "./assets/images/slideshow/slide1.jpg",
+    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+  },
+  {
+    image: "./assets/images/slideshow/slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "./assets/images/slideshow/slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "./assets/images/slideshow/slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
+];
 
 // Variables
+const arrowLeft = document.querySelector(".arrow_left");
+const arrowRight = document.querySelector(".arrow_right");
+const bannerDots = document.querySelector(".dots");
+const bannerImg = document.querySelector(".banner-img");
+const bannerParagraph = document.querySelector("#banner p");
 
-const arrowLeft = document.querySelector(".arrow_left")
-const arrowRight = document.querySelector(".arrow_right")
-const bannerDots = document.querySelector(".dots")
-const bannerImg = document.querySelector(".banner-img")
-const bannerParagraph = document.querySelector("#banner p")
-let i = 0
+const renderSlide = (index, slides) => {
+  bannerImg.src = slides[index].image;
+  bannerParagraph.innerHTML = slides[index].tagLine;
+};
 
 // Event Listener
-
+let slideIndex = 0;
 arrowLeft.addEventListener("click", () => {
-	i--
-	if (i === -1) {
-	  i = slides.length
-	}
-	bannerImg.src = slides[i].image
-	bannerParagraph.innerHTML = slides[i].tagLine
+  slideIndex--;
+  if (slideIndex === -1) {
+    slideIndex = slides.length - 1;
+  }
+  renderSlide(slideIndex, slides);
+  movingDot(slideIndex);
 });
 
 arrowRight.addEventListener("click", () => {
-	i++
-	if (i === slides.length) {
-	  i = 0
-	}
-	bannerImg.src = slides[i].image;
-	bannerParagraph.innerHTML = slides[i].tagLine
+  slideIndex++;
+  if (slideIndex === slides.length) {
+    slideIndex = 0;
+  }
+  renderSlide(slideIndex, slides);
+  movingDot(slideIndex);
 });
 
-
-
 // Création des bullets points
-for (let x = 0; x < slides.length; x++) {
-	const dot = document.createElement("span")
-	dot.classList.add("dot")
-	bannerDots.appendChild(dot)
+slides.forEach(() => {
+  const dot = document.createElement("span");
+  dot.classList.add("dot");
+  bannerDots.appendChild(dot);
+});
+
+// Selection de tout les .dot et ajout de la class .dot_selected au premier bullet point
+const addDot = document.querySelectorAll(".dot");
+addDot[0].classList.add("dot_selected");
+
+// Mise à jour des bullets points
+function movingDot(position) {
+  addDot.forEach((dot, slideIndex) => {
+    if (slideIndex === position) {
+      dot.classList.add("dot_selected");
+    } else {
+      dot.classList.remove("dot_selected");
+    }
+  });
 }
-
-
-
-
-
-
-
-
-// Non Fonctionnel
-
-// let arrow = document.querySelectorAll(".arrow")
-
-// arrow.addEventListener("click", () => {
-// 	console.log("Left click")
-// })
-
-// arrow.addEventListener("contextmenu", () => {
-// 	console.log("Right click")
-// })
-
-
-
-// Fonctionnel
-
-// let left_LeftClick = document.querySelector(".arrow_left")
-// left_LeftClick.addEventListener("click", () => {
-// 	console.log("Left click")
-// })
-
-// let left_RightClick = document.querySelector(".arrow_left")
-// left_RightClick.addEventListener("contextmenu", () => {
-// 	console.log("Right click")
-// })
-
-// let right_LeftClick = document.querySelector(".arrow_right")
-// right_LeftClick.addEventListener("click", () => {
-// 	console.log("Left click")
-// })
-
-// let right_RightClick = document.querySelector(".arrow_right")
-// right_RightClick.addEventListener("contextmenu", () => {
-// 	console.log("Right click")
-// })
-
-
-// Fonctionnel
-
-// let left_LeftClick = document.querySelector(".arrow_left").addEventListener("click", () => {
-// 	console.log("Left click")
-// })
-
-// let left_RightClick = document.querySelector(".arrow_left").addEventListener("contextmenu", () => {
-// 	console.log("Right click")
-// })
-
-// let right_LeftClick = document.querySelector(".arrow_right").addEventListener("click", () => {
-// 	console.log("Left click")
-// })
-
-// let right_RightClick = document.querySelector(".arrow_right").addEventListener("contextmenu", () => {
-// 	console.log("Right click")
-// })
